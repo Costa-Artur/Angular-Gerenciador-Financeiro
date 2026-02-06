66444597553a5dd0ee20695dd921be59fedce9ad
souza.artur <artur.souza@allog.com.br>
2026-02-06 15:17:46 -0300

    Implement initial layout and routing for the finance manager application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('../app/features/home/routes'),
+  }
 
 ];
--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatToolbarModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
